4d314986c90d1c899d5ef8388b587864af171229
Luis <luisernestoperezbello@gmail.com>
2024-04-22 06:21:50 -0600

    inegrate new elements 2

--- api_consume_app/views.py
+++ api_consume_app/views.py
@@ -18,6 +18,13 @@ class consume_api(View):
         FALLA_RENAL = request.POST.get('FALLA_RENAL') 
         ALCOHOLISMO = request.POST.get('ALCOHOLISMO') 
         TABAQUISMO = request.POST.get('TABAQUISMO') 
+        EDADMAYOR = request.POST.get('EDADMAYOR')
+        FACTORES  = request.POST.get("FACTORES")
+        AAS = request.POST.get('AAS')
+        ANGINAGRAVE = request.POST.get('ANGINAGRAVE')
+        ST = request.POST.get('ST')
+        MARCADORPOSITIVO = request.POST.get('MARCADORPOSITIVO')
+        CAD = request.POST.get('CAD') 
         
         datos_api = {
             'EDAD':EDAD ,
@@ -31,6 +38,16 @@ class consume_api(View):
             'TABAQUISMO': TABAQUISMO
         }
         
+        timi_result = {
+            'EDADMAYOR':EDADMAYOR,
+            'FACTORES':FACTORES,
+            'AAS':AAS,
+            'ANGINAGRAVE':ANGINAGRAVE,
+            'ST':ST,
+            'MARCADORPOSITIVO':MARCADORPOSITIVO,
+            'CAD':CAD,
+        }
+        
         respuesta = requests.post('http://54.145.45.183:8000/api/prediction/',json=datos_api)
         if respuesta.status_code == 200:
             
@@ -39,9 +56,22 @@ class consume_api(View):
             resultado = resultado['Nivel de riesgo']
             
             resultado= resultado * 2.17
-            context = {"resultado": resultado}
             
             print(f'{resultado}')
+            print(f"this is the values{timi_result.values()}")
+            
+            timi_result_valules = 0
+            for x in timi_result.values():
+                timi_result_valules += int(x)
+                
+            print(f"this is the values {timi_result_valules}") 
+            
+            context = {
+                "resultado": resultado,
+                'timi_result_valules':timi_result_valules
+                }
+            
+            
             return render(request,'test.html',context)
         else :
             return render(request, 'error.html',{'error',respuesta.status_code})
